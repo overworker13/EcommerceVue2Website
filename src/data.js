81e4203afb8e7591fb2383afefc34d4f8d8652fb
overworker13 <oversmile13@gmail.com>
2023-10-01 01:35:11 +0600

    Second commit

---
+++ src/data.js
@@ -0,0 +1,3 @@
+const data = { message: 'message' };
+
+export default data;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import data from './data';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+alert(data.message);
